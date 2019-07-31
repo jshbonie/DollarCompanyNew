4074ebbc115c529c131f8c4c62f5568de6087684
jshbonie <46771907+jshbonie@users.noreply.github.com>
2019-07-31 16:39:29 -0400

    Finish splash screen

---
+++ DollarCompany/DollarCompany/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DollarCompany
+{
+    public static class Program
+    {
+        public static SelectForm selectForm;
+        public static SplashScreen splashScreen;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            selectForm = new SelectForm();
+            splashScreen = new SplashScreen();
+
+            Application.Run(splashScreen);
+        }
+    }
+}
